d8de95752a67b3d78df1e25fe1f6ccd1a3064df6
ArthurWSL <arthurhcbp@gmail.com>
2021-03-29 19:33:03 -0300

    first commit

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <article>
+      <nuxt-content :document="article" />
+    </article>
+    <NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    console.log($content('articles', params.slug), 'AAAAAAAAAA')
+    const article = await $content('articles', params.slug).fetch()
+
+    return {
+      article,
+    }
+  },
+}
+</script>
+
+<style></style>
